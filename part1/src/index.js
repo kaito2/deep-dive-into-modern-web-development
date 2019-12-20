88db7e760d863f31d396b9a95b3b40180ccd3d47
Kaito Iwatsuki <kaito.iwatsuki@nislab.doshisha.ac.jp>
2019-12-20 20:27:09 +0900

    a. Introduction to React

--- part1/src/index.js
+++ part1/src/index.js
@@ -1,10 +1,33 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-const App = () => (
-  <div>
-    <p>Hello world</p>
-  </div>
-)
+const Hello = (props) => {
+    return (
+        <div>
+            <p>Hello {props.name}</p>
+        </div>
+    )
+}
+
+const Footer = () => {
+    return (
+        <div>
+            greeting app created by <a href="https://github.com/kaito2">kaito2</a>
+        </div>
+    )
+}
+
+const App = () => {
+    const name1 = "hoge"
+    return (
+        <div>
+            <h1>Greeting!!!</h1>
+            <Hello name={name1} />
+            <Hello name="huga" />
+            <Hello name="piyo" />
+            <Footer />
+        </div>
+    )
+}
 
 ReactDOM.render(<App />, document.getElementById('root'))
