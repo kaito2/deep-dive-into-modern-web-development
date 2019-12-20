c1fb988b9a1807451d50d7cff9855a87dc71b47f
Kaito Iwatsuki <kaito.iwatsuki@nislab.doshisha.ac.jp>
2019-12-20 21:49:22 +0900

    implement refresh

--- part1-c/src/index.js
+++ part1-c/src/index.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const App = (props) => {
+    const {counter} = props
+    return (
+        <div>{counter}</div>
+    )
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+let counter = 1
+
+/*
+// > Making repeated calls to the ReactDOM.render-method is not the recommended way to re-render components.
+const refresh = () => {
+    ReactDOM.render(<App counter={counter} />,
+        document.getElementById('root'))
+}
+
+setInterval(() => {
+    refresh()
+    counter += 1
+}, 1000)
+*/
+
