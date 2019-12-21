7db89ab868422fab62dce5e61dad542cafc40704
Kaito Iwatsuki <kaito.iwatsuki@nislab.doshisha.ac.jp>
2019-12-21 14:00:44 +0900

    implement frame

--- part01/ex-1-6/src/index.js
+++ part01/ex-1-6/src/index.js
@@ -1,12 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+const FeedbackStatistics = () => {
+    return (
+        <div>
+            statistics
+        </div>
+    )
+}
+
+const FeedbackInput = () => {
+    return (
+        <div>input</div>
+    )
+}
+
+const Feedback = () => {
+    return (
+        <div>
+            <FeedbackInput />
+            <FeedbackStatistics />
+        </div>
+    )
+}
+
+const App = () => {
+    const [good, setGood] = useState(0);
+    const [neutral, setNeutral] = useState(0);
+    const [bad, setBad] = useState(0);
+
+    return (
+        <div>
+            <Feedback />
+        </div>
+    )
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
