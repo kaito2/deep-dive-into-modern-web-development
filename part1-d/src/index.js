2836478c170f17bf885c981a824bd4170189c11a
Kaito Iwatsuki <kaito.iwatsuki@nislab.doshisha.ac.jp>
2019-12-21 12:57:33 +0900

    use object

--- part1-d/src/index.js
+++ part1-d/src/index.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 
+// Straightforward implementation
+/*
 const App = () => {
     const [left, setLeft] = useState(0);
     const [right, setRight] = useState(0);
@@ -16,6 +18,40 @@ const App = () => {
                     right
                 </button>
                 {right}
+            </div>
+        </div>
+    )
+}
+*/
+
+const App = (props) => {
+    const [clicks, setClicks] = useState({
+        left: 0, right: 0
+    })
+
+    const handleLeftClick = () => {
+        const newClicks = {
+            left: clicks.left + 1,
+            right: clicks.right
+        };
+        setClicks(newClicks);
+    }
+
+    const handleRightClick = () => {
+        const newClicks = {
+            left: clicks.left,
+            right: clicks.right + 1
+        }
+        setClicks(newClicks)
+    }
+
+    return (
+        <div>
+            <div>
+                {clicks.left}
+                <button onClick={handleLeftClick}>left</button>
+                <button onClick={handleRightClick}>right</button>
+                {clicks.right}
             </div>
         </div>
     )
