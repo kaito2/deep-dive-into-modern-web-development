76ed8f1b77587e6a11cd7c9c2692f2f7aac6f3fd
Kaito Iwatsuki <kaito.iwatsuki@nislab.doshisha.ac.jp>
2019-12-21 13:45:27 +0900

    aaa

--- part1-d-02/src/index.js
+++ part1-d-02/src/index.js
@@ -1,12 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+/*
+const App = () => {
+    // these are ok
+    const [age, setAge] = useState(0);
+    const [name, setName] = useState('Juha Tauriainen');
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+    if ( age > 10 ) {
+        // this does not work !!
+        const [foobar, setFoobar] = useState(null);
+    }
+
+    for ( let i = 0; i < age; i++ ) {
+        // alse this not good
+        const [rightWay, setRightWay] = useState(false);
+    }
+
+    const notGood = () => {
+        // and this is also illegal
+        const [x, setX] = useState(-1000);
+    }
+
+    return (
+        // ...
+    )
+}
+*/
+
+const App = () => {
+    const [value, setValue] = useState(10);
+
+    return (
+        <div>
+            {value}
+            <button onClick={() => setValue(0)}>reset to zero</button>
+        </div>
+    )
+}
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+);
