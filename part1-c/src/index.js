b281b4aa42c773e44639339700b1e7c396668b20
Kaito Iwatsuki <kaito.iwatsuki@nislab.doshisha.ac.jp>
2019-12-21 11:22:50 +0900

    useState

--- part1-c/src/index.js
+++ part1-c/src/index.js
@@ -1,14 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 
 const App = (props) => {
-    const {counter} = props
+    const [counter, setCounter] = useState(0)
+
+    setTimeout(
+        () => setCounter(counter + 1),
+        1000
+    )
+
+    console.log('rendering...', counter)
+
     return (
         <div>{counter}</div>
     )
 }
 
-let counter = 1
+ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+)
+
+// let counter = 1
 
 /*
 // > Making repeated calls to the ReactDOM.render-method is not the recommended way to re-render components.
