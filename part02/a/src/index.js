ea51c55469e03a6cd362d0dc227c4c4d57ca18ed
Kaito Iwatsuki <kaito.iwatsuki@nislab.doshisha.ac.jp>
2019-12-29 00:33:24 +0900

    hoge

--- part02/a/src/index.js
+++ part02/a/src/index.js
@@ -4,39 +4,53 @@ import './index.css';
 
 const notes = [
     {
-      id: 1,
-      content: 'HTML is easy',
-      date: '2019-05-30T17:30:31.098Z',
-      important: true
+        id: 1,
+        content: 'HTML is easy',
+        date: '2019-05-30T17:30:31.098Z',
+        important: true
     },
     {
-      id: 2,
-      content: 'Browser can execute only Javascript',
-      date: '2019-05-30T18:39:34.091Z',
-      important: false
+        id: 2,
+        content: 'Browser can execute only Javascript',
+        date: '2019-05-30T18:39:34.091Z',
+        important: false
     },
     {
-      id: 3,
-      content: 'GET and POST are the most important methods of HTTP protocol',
-      date: '2019-05-30T19:20:14.298Z',
-      important: true
+        id: 3,
+        content: 'GET and POST are the most important methods of HTTP protocol',
+        date: '2019-05-30T19:20:14.298Z',
+        important: true
     }
-  ]
+]
 
-  const App = (props) => {
-      const { notes } = props
+const Note = ({ note }) => {
+    return (
+        <li>{note.content}</li>
+    )
+}
 
-      return (
-          <div>
-              <h1>Notes</h1>
-              <ul>
-                  <li>{notes[0].content}</li>
-                  <li>{notes[1].content}</li>
-                  <li>{notes[2].content}</li>
-              </ul>
-          </div>
-      )
-  }
+const App = (props) => {
+    const { notes } = props
+
+    // Anti-pattern: array indexes as keys
+    const rows = () => notes.map(note =>
+        // Note, that the key attribute must now be defined for the Note components,
+        // and not for the li tags like before.
+        <Note
+            key={note.id}
+            note={note}
+        />
+    )
+
+    return (
+        <div>
+            <h1>Notes</h1>
+            <ul>
+                {rows()}
+            </ul>
+        </div>
+    )
+}
 
 ReactDOM.render(
     <App notes={notes} />,
